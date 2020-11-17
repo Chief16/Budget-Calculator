cecb8a73f89bd7aca57f442121952c272ed41e35
Chief16 <panchalakash37@gmail.com>
2020-11-17 20:31:07 +0530

    new updated

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/bgtcalc"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/bgtcalc/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/bgtcalc/src/index.html"));
 });
 app.listen(process.env.PORT || 8080);
